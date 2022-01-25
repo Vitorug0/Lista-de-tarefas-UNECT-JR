e031df58bda84d2eb6b19fcaa36136736fc20946
Vitor Gomes <vitorsabre@gmail.comgit commit -m primeiro>
2022-01-25 12:31:30 -0300

    Projeto unect-jr

--- script.js
+++ script.js
@@ -1,79 +1,119 @@
-var imputnovatarefa = window.document.getElementById('novatarefa')
-var btnaddtarefa = window.document.getElementById('btnaddtarefa')
-var listatarefas = window.document.getElementById('listatarefas')
+let rootElement = document.getElementById('root')
 
+let inputNovaTarefa = document.getElementById("inputNovaTarefa")
+let buttonNovaTarefa = document.getElementById("buttonNovaTarefa")
 
-btnaddtarefa.addEventListener('click', (e) => {
-    var tarefa = {
-        nome: imputnovatarefa.value,
-        id: gerarid(),
+let cardToDo = document.getElementById('cardToDo')
+let cardDoing = document.getElementById('cardDoing')
+let cardDone = document.getElementById('cardDone')
 
+let listaDoing = {nome: "listaDoing", proximo: "listaDone", ImagemBotao: "Imagens/certo.png", itens: []}
+let listaDone = {nome: "listaDone", proximo: "listaToDo", ImagemBotao: "Imagens/turn.png", itens: []}
+let listaToDo = {nome: "listaToDo", proximo: "listaDoing", ImagemBotao: "Imagens/seta.png", itens: []}
+
+let qtdTarefas = 0
+
+let relacionamentoListaComCard = [
+    {lista: listaToDo, card: cardToDo},
+    {lista: listaDoing, card: cardDoing},
+    {lista: listaDone, card: cardDone}
+]
+
+const renderizarTela = () => {
+    relacionamentoListaComCard.forEach(
+        relacionamento => {
+            renderizarLista(relacionamento.lista, relacionamento.card)
+        }
+    )
+}
+
+const moverParaLista = (item, origem, destino) => {
+    let itemAtual = encontrarItem(item, origem)
+    excluir(item, origem)
+    adicionarItem(itemAtual, destino)
+    
+    renderizarTela()
+}
+
+const excluir = (item, lista) => {
+    let itemAtual = encontrarItem(item, lista)
+    lista.itens.splice(lista.itens.indexOf(itemAtual), 1)
+    
+    renderizarTela()
+}
+
+const adicionarItem = (item, lista) => {
+    lista.itens.push(item)
+}
+
+const encontrarItem = (id, lista) => (
+    lista.itens.find(item => item.id == id)
+)
+
+const renderizarLista = (lista, card) => {
+    let htmlGerado = ""
+    lista.itens.forEach(
+        tarefa => {
+            htmlGerado += `
+            <li id="${lista.nome}${tarefa.id}" style="font-size: 20px; font-family: Arial; " class="list-group-item d-flex justify-content-between align-items-center">
+                ${tarefa.nome}
+                <div>
+                    <img src = "${lista.ImagemBotao}"alt="Fazer" onclick = "moverParaLista(${tarefa.id}, ${lista.nome}, ${lista.proximo})" style="position: absolute; left: 370px;"/>
+                    <img src = "Imagens/negativ.png" onclick = "excluir (${tarefa.id}, ${lista.nome})" style="position: absolute; left: 400px; " />
+                </div>
+            </li>
+            `
+        }
+    )
+    card.innerHTML = htmlGerado
+}
+
+renderizarTela()
+
+buttonNovaTarefa.addEventListener('click', (e) => {
+    if (inputNovaTarefa.value == "") return;
+
+    qtdTarefas++
+    let novaTarefa = {
+        id: qtdTarefas,
+        nome: inputNovaTarefa.value
     }
-    adicionartarefa(tarefa)
+    
+    adicionarItem(novaTarefa, listaToDo)
+    renderizarTela()
+    inputNovaTarefa.value = ""
 })
 
+//----------------------------------------------------------  
 
-function gerarid() {
-    return Math.floor(Math.random() * 3000)
-}
-
-function adicionartarefa(tarefa) {
-    var li = criartagli(tarefa)
-    listatarefas.appendChild(li)
-    imputnovatarefa.value = ''
-
-}
-
-function criartagli(tarefa) {
-    var li = document.createElement('li')
-    li.id = tarefa.id
-    var span = document.createElement('span')
-    var div = document.createElement('div')
-    var btnfazer = document.createElement('button')
+//----------------------------------------------------------
+//Ação de abrir o modal   
+function acaomodal(){
+    let modal = document.querySelector('.Modal')
+    modal.style.display='block'
     
-    span.classList.add('textotarefa')
-    li.classList.add('list-group-item')
-    span.innerHTML = tarefa.nome
+
     
-    var btnexluir = document.createElement('button')
-    btnexluir.classList.add('btnacao')
-    btnexluir.innerHTML = `<img src="Imagens/negativ.png " alt=" negativo">`
-    btnexluir.setAttribute('onclick', 'excluir('+tarefa.id+')')
-
-    btnfazer.classList.add('btnacao')
-    btnfazer.innerHTML = `<img src="Imagens/seta.png "alt="seta">`
-    btnfazer.setAttribute('onclick','passar('+tarefa.id+')')
-
-
-
-
-
-    div.appendChild(btnfazer)
-    div.appendChild(btnexluir)
-
-    li.appendChild(span)
-    li.appendChild(div)
-    return li
 }
-
-function excluir(idtarefa){
-    var confirmação = window.confirm('Tem certeza que quer excluir está tarefa?')
-    if(confirmação){
-        let li = document.getElementById(''+ idtarefa+'')
-        if(li){
-            listatarefas.removeChild(li)
-        }
-    }
+function fecharmodal(){
+    let modal = document.querySelector('.Modal')
+    modal.style.display='none'
 }
+//----------------------------------------------------------
 
-function passar(idtarefa){
-    var lidoig = document.getElementById('btndoing')
-    if(li){
-        listatarefas.appendChild()
-    }
+//Ação de colocar em dark mode
+let btn = document.getElementById("btndark")
+
+btndark.addEventListener("change", (e)=>{
+    document.body.classList.toggle("dark", e.target.checked)
+})
+//----------------------------------------------------------
 
 
-}
+
+
+
+
 
 
 
